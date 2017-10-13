f41cdf667d9040cd4013ecfd5d7fe022ce51f84e
Pavel Jakl <pavel.jakl@accolade.com>
2017-10-13 14:08:24 +0200

    AZ-43 - Adding typescript typings

---
+++ index.d.ts
@@ -0,0 +1,63 @@
+/// <reference types="express-session" />
+
+import express = require('express');
+
+declare namespace passport_openidconnect {
+
+    class AcpStrategy {
+
+        constructor(options: StrategyOptions, callback: strategyCallBackFunction);
+
+        authenticate(req: express.Request, options: StrategyOptions): any;
+
+        authorizationParams(options: StrategyOptions): any;
+
+        configure(identifier: Function, done: Function): any;
+
+    }
+
+    type strategyCallBackFunction = (req: express.Request,
+                                     iss: any,
+                                     sub: any,
+                                     profile?: OICProfile,
+                                     accessToken?: string,
+                                     refreshToken?: string,
+                                     callback?: (err: string, user: any, info: string) => void) => void;
+
+    interface OICProfile {
+        id: string,
+        displayName: string;
+        name: ProfileName,
+        _raw: string
+        _json: any;
+    }
+
+    interface ProfileName {
+        familyName: string,
+        givenName: string,
+        middleName: string
+    }
+
+    interface StrategyOptions {
+        authorizationURL: string,
+        tokenURL: string,
+        userInfoURL: string,
+        clientID: string,
+        clientSecret: string,
+        callbackURL: string,
+        scope: string,
+        passReqToCallback?: boolean,
+        prompt?: string
+    }
+
+
+    function config(fn: any): void;
+
+    function disco(fn: any): void;
+
+    function register(fn: any): void;
+
+    function registration(options: any, save: any): any;
+}
+export = passport_openidconnect;
+
